908e64180ebe3133b62d95e9667d93b67df79d9f
Rodion Sokolov <rodion.sokolov@vmssoftware.com>
2025-05-16 00:09:18 +0200

    Currently main is populated by tests

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include "dice.h"
+#include "creatures.h"
 
 int main() {
     srand(time(NULL));
-    printf("%d, %d, %d, %d, %d, %d, %d\n", roll_dice(D100), roll_dice(D20), roll_dice(D12), roll_dice(D10), roll_dice(D8), roll_dice(D6), roll_dice(D4));
+    printf("%d, %d, %d, %d, %d, %d, %d, %d\n", roll_dice(D100), roll_dice(D20), roll_dice(D12), roll_dice(D10), roll_dice(D8), roll_dice(D6), roll_dice(D4), roll_dice(D2));
+
+    print_sheet(create_creature());
 }
