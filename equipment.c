c60072a63d9b03696c3428b2f63a535bdf05254f
Rodion Sokolov <rodion.sokolov@vmssoftware.com>
2025-05-18 19:48:03 +0200

    Added equipment initial implementation

---
+++ equipment.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "equipment.h"
+#include "dice.h"
+
+eq_list *initialize_eq_list() {
+    eq_list *pointer = calloc(1, sizeof(eq_list));
+    return pointer;
+}
+
+int add_node_eq_list(eq_list *node) {
+    if (node == NULL)
+        return 1;
+    
+    for (; node->next != NULL; node = node->next);
+    
+    node->next = initialize_eq_list();
+    return 0;
+}
+
+int free_eq_list(eq_list *node) {
+    if (node == NULL)
+        return 1;
+    
+    eq_list *tmp;
+    while (node != NULL) {
+        tmp = node;
+        node = node->next;
+        free(tmp);
+    }
+    
+    free(node);
+}
+
+weapon conctrucor_weapon(unsigned dice, unsigned bonus, unsigned range[], unsigned type[], unsigned price) {
+    weapon new;
+
+    new.damage_dice = dice;
+    new.weapon_bonus = 0;
+
+    new._range[CLOSE] = range[CLOSE];
+    new._range[NEAR] = range[NEAR];
+    new._range[FAR] = range[FAR];
+
+    new._type[MELEE] = type[MELEE];
+    new._type[RANGED] = type[RANGED];
+    
+    new.price = price;
+
+    return new;
+}
+
+
+weapon basic_unarmed(unsigned dice) {
+    return conctrucor_weapon(D4, 0, CLOSE_RANGED_WEAPON, MELEE_WEAPON, 0);
+}
---
+++ equipment.h
@@ -0,0 +1,71 @@
+#ifndef SD_EQUIPMENT
+#define SD_EQUIPMENT
+
+typedef enum eq_type {
+    WEAPON,
+    ARMOR,
+    GEAR
+} eq_type;
+
+typedef enum range {
+    CLOSE,
+    NEAR,
+    FAR
+} range;
+
+#define CLOSE_RANGED_WEAPON (unsigned[]){1, 0, 0}
+
+/* Weapon type */
+typedef enum w_type {
+    MELEE,
+    RANGED
+} w_type;
+
+#define MELEE_WEAPON (unsigned[]){1, 0}
+#define RANGED_WEAPON (unsigned[]){0, 1}
+#define HYBRID_WEAPON (unsigned[]){1, 1}
+
+typedef struct gear {
+
+} gear;
+
+typedef struct armor {
+    /* Main properties */
+    unsigned int ac;
+    unsigned int DEX_cap;
+
+    /* Additional */
+    unsigned char stealth_dis;
+    unsigned char no_swim;
+    unsigned char mithral;
+
+    unsigned int price;
+    unsigned int geat_slots;
+} armor;
+
+typedef struct weapon {
+    unsigned int damage_dice;
+    unsigned int weapon_bonus;
+
+    unsigned int _range[3];
+    unsigned int _type[2];
+
+    unsigned int price;
+} weapon;
+
+
+typedef struct equipment {
+    eq_type type;
+    armor _armor;
+    weapon _weapon;
+} equipment;
+
+typedef struct eq_list
+{
+    equipment node;
+    equipment *next;
+} eq_list;
+
+extern weapon basic_unarmed(unsigned dice);
+
+#endif
