b374dd5fb418b399e83888824703ea7c97ca8fef
Rodion Sokolov <rodion.sokolov@vmssoftware.com>
2025-05-18 19:49:04 +0200

    Added constuctor for Player Charactes and races

--- creatures.c
+++ creatures.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "creatures.h"
+#include "equipment.h"
 #include "dice.h"
+
+ #define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
 
 unsigned int roll_stat() {
     return roll_d6() + roll_d6() + roll_d6();
 }
 
+int count_modifier(unsigned stat){
+    return ((int)stat-10)/2;
+}
+
 creature create_creature() {
     creature new;
-    new.aligment = roll_d10() - 1;
     new.stat[STR] = roll_stat();
     new.stat[DEX] = roll_stat();
     new.stat[CON] = roll_stat();
@@ -17,14 +26,74 @@ creature create_creature() {
     new.stat[WIS] = roll_stat();
     new.stat[CHA] = roll_stat();
 
+    new.mod[STR] = count_modifier(new.stat[STR]);
+    new.mod[DEX] = count_modifier(new.stat[DEX]);
+    new.mod[CON] = count_modifier(new.stat[CON]);
+    new.mod[INT] = count_modifier(new.stat[INT]);
+    new.mod[WIS] = count_modifier(new.stat[WIS]);
+    new.mod[CHA] = count_modifier(new.stat[CHA]);
+
     return new;
+}
+
+static unsigned establish_hit_dice(unsigned class) {
+    switch (class)
+    {
+    case FIGHTER:
+        return D8;
+    case PRIEST:
+        return D6;
+    case THIEF:
+        return D4;
+    case WIZARD:
+        return D4;
+    default:
+        return D6;
+    }
+}
+
+static unsigned roll_hp(unsigned dice, unsigned con) {
+    return max(1, roll_dice(dice) + con);
+}
+
+creature create_player_character(unsigned aligment, unsigned race, unsigned class) {
+    creature new = create_creature();
+    
+    new.race = race;
+    new.class = class;
+    new.aligment = aligment;
+
+    new.hit_dice = establish_hit_dice(class);
+    new.hp = (int) roll_hp(new.hit_dice, new.mod[CON]);
+    new.cur_hp = new.hp;
+
+    new.basic_ac = 10 + new.mod[DEX];
+    new.unarmed = basic_unarmed(D4);
+
+    new.equip = NULL;
+
+    return new;
+}
+
+void creature_attack(creature attacker, weapon attack_weapon, creature *defender) {
+    int damage;
+    if ((attacker.mod[STR] + roll_d20()) > defender->basic_ac) {
+        printf("Hit!\n");
+        damage = (int) (attacker.mod[STR] + roll_dice(attack_weapon.damage_dice) + attack_weapon.weapon_bonus);
+        defender->cur_hp -= damage;
+    } else {
+        printf("Miss!\n");
+    }
 }
 
 void print_sheet(creature someone) {
     print_alligment(someone.aligment);
-    printf("STATS:\nSTR: %d\nDEX: %d\nCON: %d\nINT: %d\nWIS: %d\nCHA: %d\n", someone.stat[STR], 
-                                                                                            someone.stat[DEX], someone.stat[CON], someone.stat[INT], 
-                                                                                            someone.stat[WIS], someone.stat[CHA]);
+    printf("STATS:\nSTR: %d\nDEX: %d\nCON: %d\nINT: %d\nWIS: %d\nCHA: %d\n\n", someone.stat[STR], someone.stat[DEX], someone.stat[CON], 
+                                                                                        someone.stat[INT], someone.stat[WIS], someone.stat[CHA]);
+    printf("||||||||||||||||||||||||||||||||||||||||\n");
+    printf("MODS:\nSTR: %d\nDEX: %d\nCON: %d\nINT: %d\nWIS: %d\nCHA: %d\n\n", someone.mod[STR], someone.mod[DEX], someone.mod[CON], 
+                                                                                        someone.mod[INT], someone.mod[WIS], someone.mod[CHA]);
+    printf("HIT DICE: %d\nHP: %d -- CURRENT HP: %d\n", someone.hit_dice, someone.hp, someone.cur_hp);
 }
 
 void print_alligment(unsigned int aligment) {
--- creatures.h
+++ creatures.h
@@ -1,16 +1,13 @@
 #ifndef SD_CREATURES
 #define SD_CREATURES
 #include <stdlib.h>
+#include "equipment.h"
 
 /* Aligment has value from 0 to 10*/
 #define LAWFUL 1
 #define NEUTRAL 5
 #define CHAOTIC 9
-
-typedef struct creature {
-    unsigned int aligment;
-    unsigned int stat[6];
-} creature;
+#define UNALIGNED 0
 
 typedef enum stats {
     STR,
@@ -21,6 +18,15 @@ typedef enum stats {
     CHA
 } stats;
 
+typedef enum races {
+    DWARF,
+    ELF,
+    GOBLIN,
+    HALFLING,
+    HALFORC,
+    HUMAN
+} races;
+
 typedef enum classes {
     FIGHTER,
     PRIEST,
@@ -29,7 +35,34 @@ typedef enum classes {
     CLASSLESS
 } classes;
 
+typedef struct creature {
+    char name[64];
+
+    int hp;
+    int cur_hp;
+
+    unsigned basic_ac;
+    unsigned hit_dice;
+    
+    unsigned race;
+    unsigned class;
+    unsigned aligment;
+
+    weapon unarmed;
+
+    unsigned stat[6];
+    int mod[6];
+    /* Equppuied in hands/on body */
+    /* List of equipment */
+    eq_list *equip;
+    /* List of spells and boons*/
+} creature;
+
 extern creature create_creature();
+
+extern creature create_player_character(unsigned aligment, unsigned race, unsigned class);
+
+extern void creature_attack(creature attacker, weapon attack_weapon, creature *defender);
 
 extern void print_alligment(unsigned int aligment);
 
