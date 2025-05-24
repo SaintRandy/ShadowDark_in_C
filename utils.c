0a13928641011691c50c5b330ca806aeb8d218c9
Rodion Sokolov <rodik12.12@mail.ru>
2025-05-25 01:05:32 +0200

    macro.h renamed to utils.h Added function to check if value in array

---
+++ utils.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "utils.h"
+
+unsigned char valueinarray(unsigned val, unsigned *arr, size_t arr_s) {
+    for (size_t i = 0; i < arr_s; ++i) {
+        if (val == arr[i])
+            return 0;
+    }
+    return 1;
+}
---
+++ utils.h
@@ -0,0 +1,9 @@
+#ifndef SD_UTILS
+#define SD_UTILS
+#include <stdlib.h>
+
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+extern char valueinarray(unsigned val, unsigned *arr, size_t arr_s);
+
+#endif
